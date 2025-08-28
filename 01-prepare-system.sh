29e96b9709ac2a32cf0fbdfefbcfd32821fafe2a
andy.yi <yixingman@gmail.com>
2025-08-28 20:57:03 +0800

    禁用GPG检查，解决Kubernetes镜像源签名验证错误

--- 01-prepare-system.sh
+++ 01-prepare-system.sh
@@ -42,9 +42,8 @@ cat > /etc/yum.repos.d/kubernetes.repo << EOF
 name=Kubernetes
 baseurl=https://mirrors.aliyun.com/kubernetes/yum/repos/kubernetes-el7-x86_64/
 enabled=1
-gpgcheck=1
-repo_gpgcheck=1
-gpgkey=https://mirrors.aliyun.com/kubernetes/yum/doc/yum-key.gpg https://mirrors.aliyun.com/kubernetes/yum/doc/rpm-package-key.gpg
+gpgcheck=0
+repo_gpgcheck=0
 exclude=kubelet kubeadm kubectl
 EOF
 
